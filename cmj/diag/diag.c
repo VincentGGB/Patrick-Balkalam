804a1eb468e5413338cd2c551a6e908f34980341
Tchikibang <54626991+Tchikibang@users.noreply.github.com>
2020-03-02 19:00:32 +0100

    Update diag.c

--- cmj/diag/diag.c
+++ cmj/diag/diag.c
@@ -14,8 +14,12 @@
 /*Je vous conseille donc de bien lire les commentaires et de me poser des questions ensuite.*/
 /*Bon courage !*/
 
+int charToInt(char *numCharDiag);
+void translate(T_Position * P, char * fen);
+
 int main (int argc, char * argv[])  // Le prototype du main est vide ou comme ceci. Impossible de faire autrement
 {
+	T_Position P;
 	char nom[30];  // On stocke ici le nom du fichier Json à creer
 	char desc[256];  // On stocke ici la description du diagramme
 	int numDiag; // On récupère la valeur int du numéro de diagramme
@@ -37,10 +41,114 @@ int main (int argc, char * argv[])  // Le prototype du main est vide ou comme ce
 
 	numDiag = *argv[1] - 48; // 48 étant le code ascii de 0
 	//  Ou  = argv[1] - '0'; // Qui fonctionne aussi
-
 	printf("Numéro de diag (int) : %d\n\n",numDiag);
+	
+	if(argc == 3) // On vérifira avant de se lancer dans l'interpretation de la commande entrée par l'utilisateur qu'il y a bien trois argument (./diag.static (1) <numDiag> (2) <codeFen> (3))
+	{
+		Diag = charToInt(argv[1]);  // Appel de la fonction charToInt (voir fonction pour comprendre)
+
+		if(Diag!=0)
+			printf("Numéro de diag (int) : %d\n\n",Diag);  // On affiche le num de diag en int pour vérifier que ça marche bien
+		else
+			printf("Erreur : Numéro de diag invalide\n");  // On retourne une erreur si le chiffre rentré est improbable
+
+		translate(&P,argv[2]);  // Appel de la fonction translate
+	}
+
 
 return 0;
+}
+
+
+void translate(T_Position * P, char * fen)  // Voici un début de fontion translate, elle est à terminer, il manque encore plein de truc à vérifier
+{
+	int i;  // On défini i ici pour le recupérer en sortie de boucle
+	
+	for (i = 0; fen[i]!=' ' && fen[i]!='\0'; ++i)  // On boucle jusqu'à la fin du code fen soit juste avant le trait (Définit juste après l'espace)
+	{
+
+		switch(fen[i])  // On utilise un switch case pour traduire
+		{
+			case 'u': P->cols[i].nb = 1;  // On utilise ici la notation fléché pour remplir la taille de la tour
+				  P->cols[i].couleur = JAU ;  // Idem pour définir la couleur
+			break;
+			
+			/*La suite est à finir, c'est récurrent et facile*/
+			case 'd':
+			break;
+
+			case 't':
+			break;
+
+			case 'q':
+			break;
+
+			case 'c':
+			break;
+
+			case 'U':
+			break;
+
+			case 'D':
+			break;
+
+			case 'T':
+			break;
+
+			case 'Q':
+			break;
+
+			case 'C':
+			break;
+
+			default: printf("La chaine fen saisie n'est pas correcte");
+				 printf("La valeur %c n'est pas reconnue",fen[i]);
+			break;
+		}	
+	}
+
+	if(fen[i]==' '  && fen[i+2]=='\0')  // Une fois sorti de la boucle il faut récupérer le trait en vérifiant que l'utilisateur n'a pas rentré de la merde
+	{
+		switch(fen[i+1])
+		{
+			case 'r': P->trait = ROU; // Idem que tout à l'heure mais pour le trait
+			break;
+			/*Idem à finir et récurant*/
+			case 'R':
+			break;
+
+			case 'j':
+			break;
+
+			case 'J':
+			break;
+		}
+	}
+//TODO : Il faut gérer le fait qu'il peut y avoir des chiffre dans le code fen pour désigne le nombre de case vide
+// TODO : Il afut gérer le fait que le code fen n'est pas toujours remplit jusqu'à l'indice 48, il faut donc à la fin du code fen définir le reste des cases comme des cases VIDE
+// TODO : Si vous pensez à autre chose rajoutez le !
+}
+
+/*Cette fonction permet de lire un chiffre dans une chaine de caractère et de le retourner en int*/
+int charToInt(char *numCharDiag)  // Cette fonction fonctione pour un nombre allant de 0 à 999
+{
+	int numDiag;  // Variable dans laquelle on va stocker la valeur en int du numDiag
+
+	if(numCharDiag[1]=='\0') // On vérifie que c'est un chiffre [1-9]
+	{
+		numDiag = numCharDiag[0] - 48;
+	}
+	else if(numCharDiag[2]=='\0') // On vérifie que le nombre ne dépasse pas 99
+	{
+		numDiag = (numCharDiag[0] - 48)*10 + (numCharDiag[1]-48);
+	}
+	else if(numCharDiag[3]=='\0')  // On vérifie que le nombre ne dépasse pas 999
+	{
+		numDiag = (numCharDiag[0] - 48)*100 + (numCharDiag[1]-48)*10 + (numCharDiag[2]-48);
+	}
+	else return 0;  // On retourne 0 si plus grand que 999 ou si invalide
+
+return numDiag;
 }
 
 // TODO : Fonction qui traduit le code Fen en chiffre et couleur a rentré dans le Json
