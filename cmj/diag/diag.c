67f89808aed408058d6afda81e2b318fa6c6a2bc
Tchikibang <54626991+Tchikibang@users.noreply.github.com>
2020-03-06 13:46:47 +0100

    Update diag.c

--- cmj/diag/diag.c
+++ cmj/diag/diag.c
@@ -26,15 +26,17 @@ int main (int argc, char * argv[])  // Le prototype du main est vide ou comme ce
 	int Diag; // On récupère la valeur int du numéro de diagramme
 	printf("Veuillez entrez le nom du fichier à créer : ");  // On demande à l'utilisateur de saisir le nom du fichier comme demandé
 	fgets(nom,sizeof(nom),stdin);  // On utilise la fonction fgets car il ne doit y avoir aucun warning
+	nom[strlen(nom)-1]='\0';
 	printf("\nVeuillez rentrez la description du diagramme : ");  // On demande à l'utilisateur la description du diagramme comme demandé
 															   // Elle sera affiché dans le bas de la page web comme dans l'exemple, le site web la récupèrera du fichier Json
 															   // ELle est donc à écrire dans le fichier Json
 	fgets(desc,sizeof(desc),stdin);  // On utilise fgets car il ne faut aucun warning
+	desc[strlen(desc)-1]='\0';
 
 	printf("\nNombre d'arg : %d = argc\n\n",argc);  // argc n'est pas le numdiag, mais le nombre d'arg
 	printf("Numéro de diag (char) : %s = argv[1]\n\n",argv[1]); // argv[1] est le numéro de diag en char
 	printf("Code Fen : %s = argv[2]\n\n",argv[2]);  // On constate que le code fen se trouve dans argv[2]
-	printf("Nom Fichier : %s\n",nom);  // Les fgets fonctionnent correctement
+	printf("Nom Fichier : >>%s<<\n",nom);  // Les fgets fonctionnent correctement
 	printf("Description : %s\n",desc);  // Les fgets fontionnent correctement 
 
 	/*On peut transformer notre argv[1] soit le numéro de diag en int        */
@@ -46,7 +48,7 @@ int main (int argc, char * argv[])  // Le prototype du main est vide ou comme ce
 	
 	if(argc == 3) // On vérifira avant de se lancer dans l'interpretation de la commande entrée par l'utilisateur qu'il y a bien trois argument (./diag.static (1) <numDiag> (2) <codeFen> (3))
 	{
-		Diag = charToInt(argv[1]);  // Appel de la fonction charToInt (voir fonction pour comprendre)
+		Diag = atoi(argv[1]);  // Appel de la fonction charToInt (voir fonction pour comprendre)
 
 		if(Diag!=0)
 			printf("Numéro de diag (int) : %d\n\n",Diag);  // On affiche le num de diag en int pour vérifier que ça marche bien
@@ -65,48 +67,117 @@ return 0;
 T_Position translate(T_Position * P, char * fen)  // Voici un début de fontion translate, elle est à terminer, il manque encore plein de truc à vérifier
 {
 	int i;  // On défini i ici pour le recupérer en sortie de boucle
+	int j = 0;
 	
 	for (i = 0; fen[i]!=' ' && fen[i]!='\0'; ++i)  // On boucle jusqu'à la fin du code fen soit juste avant le trait (Définit juste après l'espace)
 	{
 
+
+		if(fen[i]>='0' && fen[i]<='9')  // Si c'est un int
+        {
+            /*if(fen[i+2]<='0' && fen[i+2]>='9')  // Si le nombre sasie est correct (deux chiffres au plus)
+            {*/
+                int fenInt;
+                int k;
+                fenInt = atoi(&fen[i]);
+
+                if(fen[i+1]>='0' && fen[i+1]<='9')  // Si c'est un chiffre
+                {
+                	printf("coucou");
+                    fenInt = fenInt*10 + atoi(&fen[i+1]);
+                    i=i+2;
+                }
+                else
+                {
+                	i=i+1;
+                }
+
+
+                for (k = j; k < fenInt+j; ++k)
+                {
+                    P->cols[k].nb = 0;
+                    P->cols[k].couleur = VIDE;
+                }
+                j=k;
+                /*P->cols[j].nb = 0;
+                P->cols[j].couleur = VIDE;*/
+           //}
+        }
+
+
 		switch(fen[i])  // On utilise un switch case pour traduire
 		{
-			case 'u': P->cols[i].nb = 1;  // On utilise ici la notation fléché pour remplir la taille de la tour
-				  P->cols[i].couleur = JAU ;  // Idem pour définir la couleur
+			case 'u': P->cols[j].nb = 1;  // On utilise ici la notation fléché pour remplir la taille de la tour
+				  P->cols[j].couleur = JAU ;  // Idem pour définir la couleur
 			break;
 			
 			/*La suite est à finir, c'est récurrent et facile*/
-			case 'd':
+			case 'd': P->cols[j].nb = 2;
+					  P->cols[j].couleur = JAU ;
 			break;
 
-			case 't':
+			case 't':P->cols[j].nb = 3;
+					 P->cols[j].couleur = JAU ;
 			break;
 
-			case 'q':
+			case 'q':P->cols[j].nb = 4;
+					 P->cols[j].couleur = JAU ;
 			break;
 
-			case 'c':
+			case 'c':P->cols[j].nb = 5;
+					 P->cols[j].couleur = JAU ;
 			break;
 
-			case 'U':
+			case 'U':P->cols[j].nb = 1;
+					 P->cols[j].couleur = ROU ;
 			break;
 
-			case 'D':
+			case 'D':P->cols[j].nb = 2;
+					 P->cols[j].couleur = ROU ;
 			break;
 
-			case 'T':
+			case 'T':P->cols[j].nb = 3;
+					 P->cols[j].couleur = ROU ;
 			break;
 
-			case 'Q':
+			case 'Q':P->cols[j].nb = 4;
+					 P->cols[j].couleur = ROU ;
 			break;
 
-			case 'C':
+			case 'C':P->cols[j].nb = 5;
+					 P->cols[j].couleur = ROU ;
 			break;
+
+			/*case '0':
+
+
+			case '1':
+
+
+			case '2':
+
+
+			case '3':
+
+
+			case '4':
+
+
+			case '5':
+	
+
+			case '6':
+		
+
+			case '7':
+			case '8': 
+			break;*/
 
 			default: printf("La chaine fen saisie n'est pas correcte");
 				 printf("La valeur %c n'est pas reconnue",fen[i]);
 			break;
 		}	
+		j++;
 	}
 
 	if(fen[i]==' '  && fen[i+2]=='\0')  // Une fois sorti de la boucle il faut récupérer le trait en vérifiant que l'utilisateur n'a pas rentré une valeure interdite
@@ -126,32 +197,16 @@ T_Position translate(T_Position * P, char * fen)  // Voici un début de fontion 
 			break;
 		}
 	}
+
+	for (int j = j; j <= 47; ++j)
+	{
+		
+	}
+
 //TODO : Il faut gérer le fait qu'il peut y avoir des chiffre dans le code fen pour désigne le nombre de case vide
 // TODO : Il afut gérer le fait que le code fen n'est pas toujours remplit jusqu'à l'indice 48, il faut donc à la fin du code fen définir le reste des cases comme des cases VIDE
 // TODO : Si vous pensez à autre chose rajoutez le !
 return *P;
-}
-
-/*Cette fonction permet de lire un chiffre dans une chaine de caractère et de le retourner en int*/
-int charToInt(char *numCharDiag)  // Cette fonction fonctione pour un nombre allant de 0 à 999
-{
-	int numDiag;  // Variable dans laquelle on va stocker la valeur en int du numDiag
-
-	if(numCharDiag[1]=='\0') // On vérifie que c'est un chiffre [1-9]
-	{
-		numDiag = numCharDiag[0] - 48;
-	}
-	else if(numCharDiag[2]=='\0') // On vérifie que le nombre ne dépasse pas 99
-	{
-		numDiag = (numCharDiag[0] - 48)*10 + (numCharDiag[1]-48);
-	}
-	else if(numCharDiag[3]=='\0')  // On vérifie que le nombre ne dépasse pas 999
-	{
-		numDiag = (numCharDiag[0] - 48)*100 + (numCharDiag[1]-48)*10 + (numCharDiag[2]-48);
-	}
-	else return 0;  // On retourne 0 si plus grand que 999 ou si invalide
-
-return numDiag;
 }
 
 void ecriture(T_Position pos, char * ficName,char * diagDesc,int numDiag)  // Même fonctionnement que pour le standalone
