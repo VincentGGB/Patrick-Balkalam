430da5f3bb3a6465f2257e2dc9dac8a73934d5df
Tchikibang <54626991+Tchikibang@users.noreply.github.com>
2020-05-18 18:10:27 +0200

    Add files via upload

--- avalam-tournoi2019v2/joueurs/joueur6.c
+++ avalam-tournoi2019v2/joueurs/joueur6.c
@@ -17,26 +17,28 @@ int testCoup(int pot,int max,int i)
 	return max;
 }
 
-int testCoup2(int pot,int max)
+int calculMin(int pot,int min)
 {
-	if(pot>max)
+	if(pot<min)
 	{
-		max=pot;
+		min=pot;
 	}
-	return max;
+	return min;
 }
 
-int coupVoisins(octet dBis,octet oBis,T_Position P)
+int test(octet dBis,octet oBis,T_Position P)
 {
 	T_Voisins voisins;
 	T_ListeCoups l;
 	voisins = getVoisins(dBis);
 	int k = 0;
 	int pot;
-	int max = -10;
+	int min = 0;
+	int cpt = 0;
 	octet o,d;
 	l.nb = 0;
 	listerVoisins(dBis);
+	int Color = P.cols[dBis].couleur;
 	P = jouerCoup(P,oBis,dBis);
 
 	printf("--------------------voisins : %d-----------------\n",voisins.nb);
@@ -45,7 +47,7 @@ int coupVoisins(octet dBis,octet oBis,T_Position P)
 	for (int j = 0; j < voisins.nb; ++j)
 	{
 		printf("nb = %d\n",P.cols[voisins.cases[j]].nb);
-		if(((P.cols[voisins.cases[j]].nb)!=0) && ((P.cols[voisins.cases[j]].nb + P.cols[voisins.cases[dBis]].nb )<= 5))
+		if(((P.cols[voisins.cases[j]].nb)!=0) && ((P.cols[voisins.cases[j]].nb + P.cols[dBis].nb )<= 5))
 		{
 			l.coups[k].origine = voisins.cases[j];
 			l.coups[k].destination = dBis;
@@ -60,53 +62,165 @@ int coupVoisins(octet dBis,octet oBis,T_Position P)
 	printf("%d",k);
 	afficherListeCoups(l);
 
-
-
-
-	for (int j = 0; j < l.nb; ++j)
+	if(l.nb == 0)
 	{
-
-		o = l.coups[j].origine; 
-		d = l.coups[j].destination;
-		if(P.cols[o].couleur != P.trait)
-		{
-			switch(P.cols[o].nb + P.cols[o].nb)
-			{
-				case 2: pot = 2;
-				break;
-
-				case 3: pot = 3;
-				break;
-
-				case 4: pot = 4;
-						//coupVoisins(d);
-				break;
-
-				case 5:pot = 5;
-				break;
-			}
-		}
-		else
-		{
-			switch(P.cols[d].nb + P.cols[d].nb)
-			{
-				case 2: pot = -2;
-
-				break;
-
-				case 3: pot = -3;
-				break;
-
-				case 4: pot = -4;
-				break;
-
-				case 5:pot = -5;
-				break;
-			}
-		}
-		max = testCoup2(pot,max);
+		min = -10;
 	}
-		return max;
+	/*else
+	{
+		for (int j = 0; j < l.nb; ++j)
+		{
+			printf("test3");
+			o = l.coups[j].origine; 
+			d = l.coups[j].destination;
+			if(P.cols[o].couleur == P.trait)
+			{
+				switch(P.cols[o].nb + P.cols[d].nb)
+				{
+					case 5:pot = 4;
+					break;
+				}
+				min = calculMin(pot,min);
+			}
+			else
+			{
+				cpt++;
+				switch(P.cols[o].nb + P.cols[d].nb)
+				{
+					case 5:pot = -10;
+					break;
+				}
+				min = calculMin(pot,min);
+			}
+		}
+	}*/
+	return min;
+
+}
+
+int coupVoisins(octet dBis,octet oBis,T_Position P)
+{
+	T_Voisins voisins;
+	T_Voisins voisins2;
+	T_ListeCoups l;
+	voisins = getVoisins(dBis);
+	voisins2 = getVoisins(oBis);
+	int k = 0;
+	int pot;
+	int min = 0;
+	int cptEn = 0;
+	int cptAm = 0;
+	int voisEn =0;
+	octet o,d;
+	l.nb = 0;
+	listerVoisins(dBis);
+	int Color = P.cols[dBis].couleur;
+
+	for (int j = 0; j < voisins2.nb; ++j)
+	{
+		printf("voiEn -> Couleur : %d\n",P.cols[voisins2.cases[j]].couleur);
+		if(((P.cols[voisins2.cases[j]].nb)!=0) && (P.cols[voisins2.cases[j]].couleur != P.trait))
+		{
+			voisEn++;
+		}
+	}
+	P = jouerCoup(P,oBis,dBis);
+
+	printf("--------------------voisins : %d-----------------\n",voisins.nb);
+	printf("--------------------voisins pos: %d-----------------\n",voisins.cases[0]);
+
+	for (int j = 0; j < voisins.nb; ++j)
+	{
+		printf("nb = %d\n",P.cols[voisins.cases[j]].nb);
+		if(((P.cols[voisins.cases[j]].nb)!=0) && ((P.cols[voisins.cases[j]].nb + P.cols[dBis].nb )<= 5))
+		{
+			l.coups[k].origine = voisins.cases[j];
+			l.coups[k].destination = dBis;
+			k++;
+			l.nb++;
+			l.coups[k].origine = dBis;
+			l.coups[k].destination = voisins.cases[j];
+			k++;
+			l.nb++;
+			if(P.cols[voisins.cases[j]].couleur != P.trait)
+			{
+				cptAm++;
+				printf("Couleur : %d",P.cols[voisins.cases[j]].couleur);
+
+			}
+			else
+			{
+				cptEn++;
+			}
+		}
+
+	}
+	printf("%d",k);
+	afficherListeCoups(l);
+
+
+	if(l.nb == 0 && Color != P.trait)
+	{
+		min = 3;
+	}
+	else
+	{
+		for (int j = 0; j < l.nb; ++j)
+		{
+
+			o = l.coups[j].origine; 
+			d = l.coups[j].destination;
+			if(P.cols[o].couleur != P.trait)
+			{
+				switch(P.cols[o].nb + P.cols[d].nb)
+				{
+					case 3: pot = 3;
+					break;
+
+					case 4: pot = 4;
+							//coupVoisins(d);
+					break;
+
+					case 5:pot = 5;
+					break;
+				}
+				min = calculMin(pot,min);
+			}
+			else
+			{
+				switch(P.cols[o].nb + P.cols[d].nb)
+				{
+					case 3: printf("test1");
+					pot = 3 + test(d,o,P);
+					break;
+
+					case 4: printf("test2");
+					pot = 3 + test(d,o,P);
+					break;
+
+					case 5:pot = -10;
+					break;
+				}
+				min = calculMin(pot,min);
+			}
+		}
+		printf("\ncptAm : %d && cptEn : %d\n",cptAm,cptEn);
+		if(cptAm < 1 && cptEn > 1)
+		{
+			min = -15;
+		}
+		else if(cptEn == 0)
+		{
+			min = 7;
+		}
+	}
+	printf("voisEn : %d",voisEn);
+	if(voisEn ==0)
+	{
+		min=-7;
+	}
+		printf("\n\nmini = %d\n\n",min);
+		return min;
 }
 
 void choisirCoup(T_Position currentPosition, T_ListeCoups listeCoups) {
@@ -137,21 +251,21 @@ void choisirCoup(T_Position currentPosition, T_ListeCoups listeCoups) {
 				ecrireIndexCoup(i);
 				return; // on quitte la fonction 
 			}*/
-
-		if(currentPosition.cols[o].couleur == myColor)
+		//Jaune sur jaune
+		if(currentPosition.cols[o].couleur == myColor && currentPosition.cols[d].couleur == myColor)
 		{
 			printf("\n\nla somme = %d\n\n",currentPosition.cols[d].nb + currentPosition.cols[d].nb);
 			switch(currentPosition.cols[o].nb + currentPosition.cols[d].nb)
 			{
-				case 2: pot = coupVoisins(d,o,currentPosition);
+				case 2: pot = 3 + coupVoisins(d,o,currentPosition);
 						printf("pot = %d",pot);
 				break;
 
-				case 3: pot = coupVoisins(d,o,currentPosition);;
+				case 3: pot = 3 + coupVoisins(d,o,currentPosition);
 				break;
 
 				case 4:
-						pot = coupVoisins(d,o,currentPosition);;
+						pot = 4 + coupVoisins(d,o,currentPosition);
 				break;
 
 				case 5:pot = 5;
@@ -160,7 +274,29 @@ void choisirCoup(T_Position currentPosition, T_ListeCoups listeCoups) {
 			max = testCoup(pot,max,i);
 			
 		}
-		else
+		// jaune sur rouge
+		else if(currentPosition.cols[o].couleur == myColor && currentPosition.cols[d].couleur != myColor)
+		{
+			switch(currentPosition.cols[o].nb + currentPosition.cols[d].nb)
+			{
+				case 2: pot = 6 + coupVoisins(d,o,currentPosition);
+						printf("pot = %d",pot);
+				break;
+
+				case 3: pot = 6 + coupVoisins(d,o,currentPosition);
+				break;
+
+				case 4:
+						pot = 5 + coupVoisins(d,o,currentPosition);
+				break;
+
+				case 5:pot = 10;
+				break;
+			}
+			max = testCoup(pot,max,i);
+		}
+		// Rouge sur rouge
+		else if(currentPosition.cols[o].couleur != myColor && currentPosition.cols[d].couleur != myColor)//TODO
 		{
 			switch(currentPosition.cols[o].nb + currentPosition.cols[d].nb)
 			{
@@ -179,9 +315,13 @@ void choisirCoup(T_Position currentPosition, T_ListeCoups listeCoups) {
 			}
 			max = testCoup(pot,max,i);
 		}
+		//rouge sur jaune (à ne pas faire)
+		else//TODO
+		{
+			
+		}
 
 	}
-	//ecrireIndexCoup(i);
  
 
 
